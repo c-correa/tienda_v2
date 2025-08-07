41b70a8e387ad382e9a85088d43871e5a5e81ab9
Developer Backend Node <160889254+c-correa@users.noreply.github.com>
2025-08-07 18:02:27 -0500

    Add Categories module and service
    Introduces the CategoriesModule and CategoriesService for managing categories. The service includes basic CRUD method stubs and is set up with TypeORM integration for Category and Product entities.

---
+++ src/categories/categories.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CategoriesController } from './categories.controller';
+import { Category } from './entities/category.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from 'src/products/entities/product.entity';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Category, Product])
+],
+  controllers: [CategoriesController],
+  providers: [CategoriesService],
+})
+export class CategoriesModule {}
