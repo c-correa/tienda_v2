c5e707329d68d73aeb17a666093662e270f283ba
Developer Backend Node <160889254+c-correa@users.noreply.github.com>
2025-08-07 18:00:03 -0500

    Initialize NestJS project with core modules
    Add initial project structure including README, main app controller, and app module. Set up core modules for authentication, users, products, categories, and cart items, and configure TypeORM with PostgreSQL and environment support.

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { CartItemsModule } from './cart_items/cart_items.module';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+import { CartItem } from './cart_items/entities/cart_item.entity';
+import { Product } from './products/entities/product.entity';
+import { Category } from './categories/entities/category.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123456789',
+      database: 'postgres',
+      entities: [User, CartItem, Product, Category,],
+      synchronize: true,
+    }),
+    AuthModule, UsersModule, CategoriesModule, ProductsModule, CartItemsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
